da7a8b8f9c03590892f5eabb77fffdb333305336
Stephen Ditta <stephenditta@Stephens-MacBook-Pro.local>
2018-02-25 22:42:52 -0500

    Added Register for Activities section //TODO need to condence

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ const otherOption = document.getElementById('other-title');
 const shirtDesign = document.getElementById('design');
 const shirtColor = document.getElementById('color');
 
+const activities = document.getElementsByClassName('activities')[0];
+
 window.addEventListener("load", function(){
   nameInput.focus(); // After the page loads the name input is focused
 
@@ -62,6 +64,59 @@ shirtDesign.addEventListener('change', function(e){
   });
 }
 
+
+
+activities.addEventListener('change', function(e){
+  // console.log(e.target);
+  const activitieCheckBox = activities.children;
+  // console.log(activitieCheckBox);
+  // console.log(e.target.checked);
+    // console.log(activitieCheckBox[i].cheZZZ);
+    if(e.target.checked){
+      console.log('Checked');
+      // if(){
+      //
+      // }
+      // console.log(e.target.name);
+      if(e.target.name === 'js-frameworks'){
+        // console.log(activitieCheckBox[4]);
+        activitieCheckBox[4].firstElementChild.disabled = true;
+        activitieCheckBox[4].style.color = '#596D75';
+      } else if (e.target.name === 'js-libs'){
+        activitieCheckBox[5].firstElementChild.disabled = true;
+        activitieCheckBox[5].style.color = '#596D75';
+      } else if (e.target.name === 'express'){
+        activitieCheckBox[2].firstElementChild.disabled = true;
+        activitieCheckBox[2].style.color = '#596D75';
+      } else if (e.target.name === 'node'){
+        activitieCheckBox[3].firstElementChild.disabled = true;
+        activitieCheckBox[3].style.color = '#596D75';
+      }
+    } else {
+      console.log('Unchecked');
+      if(e.target.name === 'js-frameworks'){
+        // console.log(activitieCheckBox[4]);
+        activitieCheckBox[4].firstElementChild.disabled = false;
+        activitieCheckBox[4].style.color = '#000';
+      } else if (e.target.name === 'js-libs'){
+        activitieCheckBox[5].firstElementChild.disabled = false;
+        activitieCheckBox[5].style.color = '#000';
+      } else if (e.target.name === 'express'){
+        activitieCheckBox[2].firstElementChild.disabled = false;
+        activitieCheckBox[2].style.color = '#000';
+      } else if (e.target.name === 'node'){
+        activitieCheckBox[3].firstElementChild.disabled = false;
+        activitieCheckBox[3].style.color = '#000';
+      }
+    //   for(let i = 1; i < activitieCheckBox.length; i++){
+    //     // if(activitieCheckBox[i].firstElementChild){
+    //     //
+    //     // }
+    //   activitieCheckBox[i].firstElementChild.disabled = false;
+    //   activitieCheckBox[i].style.color = '#000';
+    // }
+  }
+});
 
 //if checked add anount price to an array and add all together to get total
 
